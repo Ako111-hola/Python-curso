04c621bc14ce414c16e2da2e4cdf82bfafcc691d
Ako111-hola <johanmedin1133@gmail.com>
2026-07-06 00:48:36 -0500

    feat: Agregar nuevos temas en el archivo clase.py Nuevos comentarios de ayuda en archivo clase.py Actualizar README

--- POO/clase.py
+++ POO/clase.py
@@ -1,22 +1,58 @@
+#Definiendo una clase
 class Personaje:
+    #Definiendo los atributos de esta clase
     def __init__(self, tipo, vida, daño):
         self.tipo =  tipo
         self.vida = vida
         self.daño = daño
-       
+    
+    #Metodo exclusivo de esta clase
     def informacion(self):
         return f'El personaje es de tipo {self.tipo}\nTiene {self.vida} puntos de vida\ny tiene {self.daño} puntos de daño'
 
+    #GEETER metodo para mostrar el atributo del objeto
+    def get_vida(self):
+        return self.vida
+    
+    #SETTER metodo para actualizar o cambiar el atributo del objeto
+    def set_vida(self, nueva_vida):
+        self.vida = nueva_vida
+
+class NPC:
+    def __init__(self, dialogo):
+        self.dialogo = dialogo
+    
+    def hablar(self):
+        return self.dialogo
+
+#Heredando la clase Personaje en la nueva clase Villano
 class Villano(Personaje):
+    #Colocamos los atributos de la clase padre y finalmente de la clase hijo 
     def __init__(self, tipo, vida, daño, fases):
+        #Heredamos los atributos de la clase padre con la función super()
         super().__init__(tipo, vida, daño)
+        #Definimos los atributos de la clase hijo
         self.fases = fases
     
-    def informacion(self):
+    def informacionVillano(self):
         return f'El personaje es de tipo {self.tipo}\nTiene {self.vida} puntos de vida\nTiene {self.daño} puntos de daño\ny tiene {self.fases} fases'
 
-verdugo = Villano("tanque", 1000000, 1000000, 3)
-
-print(verdugo.informacion())
-
-print("hola"[-1])
+#Herencia multiple de las clases Personaje y NPC
+class NPC_Jugable(Personaje, NPC):
+    def __init__(self, tipo, vida, daño, dialogo, importancia):
+        #Heredamos los atrubutos de las clases padre. Similar a usar la función super()
+        Personaje.__init__(self, tipo, vida, daño)
+        NPC.__init__(self, dialogo)
+        
+        self.importancia = importancia
+    
+    def informacion(self):
+        return f'Hola, soy un NPC de importancia {self.importancia}'
+    
+    #self. para usar metodo de clase hijo
+    def presentacion(self):
+        return self.informacion()
+    
+    #super() para usar metodo de clase padre 
+    def presentacion1(self):
+        return super().informacion()
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 Python Basico
 
-Ignora mi fanatismo por las seiyuus
+Temas tratados:
+    Introducción:
+        Tipos de datos
+        Variables simples y compuestas
+        Operadores aritmeticos, comparativos y logicos
+        if - else
+        input
+        bucle for y while
+        Funciones basicas
+
+    Metodos:
+        M. de cadenas de texto
+        M. listas
+        M. Conjuntos
+        M. Diccionarios
+
+    Modulos:
+        importacion general y especifica
+        renombramiento
+
+    POO:
+        Definicion de clases y objetos
+        Clases con atributos
+        Definicion de metodos
+        Herencia
+        Herencia multiple
+
+ignora mi fanatismo por algunos nombres
